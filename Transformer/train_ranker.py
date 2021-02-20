09e814aca9ca4ace31565626ae8aa84b55eab1a7
Heng <zhlim98@hotmail.com>
2021-02-21 02:37:31 +0800

    train ranker with margin ranking loss

--- Transformer/train_ranker.py
+++ Transformer/train_ranker.py
@@ -9,7 +9,7 @@ import transformers
 
 from model.embedding_model import EmbeddingModel
 from model.reranker_model import SimpleReranker
-from data.dataset import QueryPairDataset
+from data.dataset import TripletDataset, TripletCollator
 from utils import distributed
 
 import json
@@ -26,19 +26,29 @@ logger.setLevel(logging.INFO)
 
 
 def train_one_epoch(
-    model, train_dataloader, criterion, optimizer, scheduler, epoch, writer, device
+    embedding_model, model, train_dataloader, criterion, optimizer, scheduler, epoch, writer, device
 ):
     logger.info(f"===Training epoch {epoch}===")
     model.train()
-    for i, (q, c, label) in enumerate(
+    embedding_model.eval()
+    for i, (q, p, n) in enumerate(
         tqdm(train_dataloader), epoch * len(train_dataloader)  # Need check
     ):
-        q = q.to(device)
-        c = c.to(device)
-        label = label.float().unsqueeze(1).to(device)
+        q, p, n = (
+            to_device_dict(q, device),
+            to_device_dict(p, device),
+            to_device_dict(n, device),
+        )
 
-        output = model(q, c)
-        loss = criterion(output, label)
+        with torch.no_grad():
+            query_embedding = embedding_model(q)
+            positive_embedding = embedding_model(p)
+            negative_embedding = embedding_model(n)
+
+
+        pos_result = model(query_embedding, positive_embedding)
+        neg_result = model(query_embedding, negative_embedding)
+        loss = criterion(pos_result, neg_result, torch.ones(pos_result.size(0)))
         loss.backward()
 
         if (i + 1) % 50 == 0:
@@ -51,30 +61,34 @@ def train_one_epoch(
 
 
 @torch.no_grad()
-def eval(model, eval_dataloader, criterion, epoch, writer, device):
+def eval(embedding_model, model, eval_dataloader, criterion, epoch, writer, device):
+    embedding_model.eval()
     model.eval()
     loss_list = []
     num_correct = 0
-    for i, (q, c, label) in enumerate(tqdm(eval_dataloader), epoch):
-        q = q.to(device)
-        c = c.to(device)
-        label = label.float().unsqueeze(1).to(device)
+    for q, p, n in tqdm(eval_dataloader):
+        q, p, n = (
+            to_device_dict(q, device),
+            to_device_dict(p, device),
+            to_device_dict(n, device),
+        )
 
-        output = model(q, c)
-        loss = criterion(output, label)
+        query_embedding = embedding_model(q)
+        positive_embedding = embedding_model(p)
+        negative_embedding = embedding_model(n)
 
-        output = output.sigmoid()
-        prediction = (output > 0.5).float()
-        num_correct += torch.sum(prediction == label).item()
 
-        if i % 50 == 0:
-            loss_list.append(loss.item())
+        pos_result = model(query_embedding, positive_embedding)
+        neg_result = model(query_embedding, negative_embedding)
+        loss = criterion(pos_result, neg_result, torch.ones(pos_result.size(0)))
 
+        loss_list.append(loss_recorded.item())
+   
     epoch_loss = torch.tensor(loss_list, dtype=torch.float).mean()
-    acc = num_correct / len(eval_dataloader.dataset)
+    # acc = num_correct / len(eval_dataloader.dataset)
 
     writer.add_scalar("val_ranker/loss", epoch_loss, epoch)
-    writer.add_scalar("val_ranker/acc", acc, epoch)
+    # writer.add_scalar("val_ranker/acc", acc, epoch)
 
 def to_device_dict(d, device):
     return {k: v.to(device) for k, v in d.items()}
@@ -98,8 +112,8 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = SimpleReranker().to(device)
-    criterion = nn.BCEWithLogitsLoss()
-    random.seed(config.seed)
+    criterion = nn.MarginRankingLoss()
+    # random.seed(config.seed)
 
     if not os.path.exists(f"weights/{config.experiment_name}"):
         os.makedirs(f"weights/{config.experiment_name}")
@@ -107,15 +121,15 @@ if __name__ == "__main__":
     with open("dblp_triplet.pkl", "rb") as f:
         unpickled_data = pickle.load(f)
 
-    # tokenizer = transformers.AutoTokenizer.from_pretrained(config.model_name)
-    # embedding_model = EmbeddingModel(config)
-    # state_dict = torch.load(
-    #     f"weights/{config.experiment_name}/weights_{config.reload_epoch}.pth", 
-    #     map_location="cuda:0"
-    # )["state_dict"]
+    tokenizer = transformers.AutoTokenizer.from_pretrained(config.model_name)
+    embedding_model = EmbeddingModel(config)
+    state_dict = torch.load(
+        f"weights/{config.experiment_name}/weights_{config.reload_epoch}.pth", 
+        map_location="cuda:0"
+    )["state_dict"]
 
-    # embedding_model.load_state_dict(state_dict)
-    # embedding_model.to(device)
+    embedding_model.load_state_dict(state_dict)
+    embedding_model = embedding_model.to(device)
 
     # embedding = torch.empty((len(unpickled_data["dataset"]), 768), dtype=torch.float)
     # with torch.no_grad():
@@ -136,38 +150,48 @@ if __name__ == "__main__":
     # torch.cuda.empty_cache()
 
     # torch.save(embedding, "all_embedding_dblp.pth")
-    embedding = torch.load("all_embedding_dblp.pth")
+    # embedding = torch.load("all_embedding_dblp.pth")
 
-    train_query_pair_dataset = QueryPairDataset(unpickled_data["train"], embedding)
-    test_query_pair_dataset = QueryPairDataset(unpickled_data["test"], embedding)
-        
-    train_query_pair_dataloader = DataLoader(
-        train_query_pair_dataset,
+    train_triplet_dataset = TripletDataset(
+        unpickled_data["train"], unpickled_data["dataset"]
+    )
+    test_triplet_dataset = TripletDataset(
+        unpickled_data["valid"], unpickled_data["dataset"]
+    )
+
+    collater = TripletCollator(tokenizer, config.max_seq_len)
+
+    train_triplet_dataloader = DataLoader(
+        train_triplet_dataset,
         batch_size=config.batch_size,
         num_workers=2,
         pin_memory=True,
-        shuffle=True
+        shuffle=True,
+        collate_fn=collater,
     )
-    test_query_pair_dataloader = DataLoader(
-        test_query_pair_dataset,
+    test_triplet_dataloader = DataLoader(
+        test_triplet_dataset,
         batch_size=config.batch_size,
         num_workers=2,
         pin_memory=True,
+        collate_fn=collater,
     )
+
 
     optimizer = optim.Adam(model.parameters(), lr=config.lr)
     scheduler = transformers.get_linear_schedule_with_warmup(
         optimizer,
-        len(train_query_pair_dataloader),
-        len(train_query_pair_dataloader) * config.epochs,
+        len(train_triplet_dataloader),
+        len(train_triplet_dataloader) * config.epochs,
     )
 
     writer = SummaryWriter(f"runs/{config.experiment_name}")
 
     for epoch in range(0, config.epochs):
         train_one_epoch(
+            embedding_model,
             model,
-            train_query_pair_dataloader,
+            train_triplet_dataloader,
             criterion,
             optimizer,
             scheduler,
@@ -175,7 +199,7 @@ if __name__ == "__main__":
             writer,
             device
         )
-        eval(model, test_query_pair_dataloader, criterion, epoch, writer, device)
+        eval(embedding_model, model, test_triplet_dataloader, criterion, epoch, writer, device)
 
         torch.save(
             {"state_dict": model.state_dict()},
